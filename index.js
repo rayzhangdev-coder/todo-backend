07dc8548d45a3cbc62ae271130248110a01a7a43
rayzhangdev-coder <ray.zhang.dev@gmail.com>
2025-12-02 11:22:42 -0500

    updated port to allow process.env.PORT instead of forcing 3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // const bodyParser = require("body-parser"); REPLACED WITH express.json()
 const cors = require('cors'); //I NEED THIS TO CONNECT BACKEND W/ FRONTEND
 const mongoose = require('mongoose');
